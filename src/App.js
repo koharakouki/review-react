2f3cecbfbf183caf1ae8030bc74727c3262da551
koharakouki <ko.kohara02@gmail.com>
2020-08-06 14:12:52 +0900

    fin chapter3-3

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
 import React, { Component } from 'react';
 // import Rect from './components/Rect'
 import './App.css';
+import { render } from '@testing-library/react';
 
 class App extends Component {
+  data = [];
 
-  constructor(props){
+  constructor(props)[
     super(props);
     this.state = {
-      counter: 0,
-      msg: "count start!",
-      flg: true
+      list: this.data
     };
     this.doAction = this.doAction.bind(this);
-  }
+  ]
 
   doAction(e){
-    this.setState((state)=>({
-      counter: state.counter + 1,
-      msg: 'count: ' + state.counter,
-      flg: !state.flg
-    }));
+    let x = e.pageX;
+    let y = e.pageY;
+    this.data.push({ x: x, y: y });
+    this.setState({
+      list: this.data
+    });
+  }
+
+  draw(d){
+    let s = {
+      position: "absolute",
+      left: (d.x - 25) + "px",
+      top: (d.y - 25) + "px",
+      width: "50px",
+      height: "50px",
+      backgroundColor: "#66f3",
+    };
+    return <div style={s}></div>
   }
 
   render(){
     return(
       <div>
         <h1>React</h1>
-    　　 { this.state.flg ? <p>count: {this.state.msg}</p> : <p>{this.state.msg}です。</p>}
-        <button onClick={this.doAction}>Click</button>
+        <h2>show. rect</h2>
+        <div onClick={this.doAction}>{this.data.map((value)=>this.draw(value))}</div>
       </div>
     );
   }
