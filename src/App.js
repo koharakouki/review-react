53b7d7c533e059b9d9d4c9ee21ecddf964166bf1
koharakouki <ko.kohara02@gmail.com>
2020-08-06 13:38:48 +0900

    add state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-import Rect from './components/Rect'
+// import Rect from './components/Rect'
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      msg: "Hello Component"
+    };
+  }
+
   render(){
     return(
       <div>
         <h1>React</h1>
-        <Rect x="50" y="50" w="150" h="150" c="cyan" />
-        <Rect x="150" y="100" w="150" h="150" c="magenta" />
-        <Rect x="100" y="150" w="150" h="150" c="black" />
+        <p>{this.state.msg}</p>
+        <p>{this.props.msg}</p>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App title="hi" message="this is message" />, document.getElementById('root'));
+ReactDOM.render(<App msg="hi" />, document.getElementById('root'));
 
 serviceWorker.unregister();
