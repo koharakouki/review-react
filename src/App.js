15ed5774921e6a978d865b402385ae7123d35294
koharakouki <ko.kohara02@gmail.com>
2020-08-10 14:58:18 +0900

    context

--- src/App.js
+++ src/App.js
@@ -2,50 +2,42 @@ import React, { Component } from 'react';
 // import Rect from './components/Rect'
 import './App.css';
 
+let data = {title: 'Title', message: 'this is sample message.'};
+
+const SampleContext = React.createContext(data);
 
 class App extends Component {
-  input = '';
-
-  constructor(props){
-    super(props);
-    this.state = {
-      message: 'type your name.'
-    };
-    this.doCheck = this.doCheck.bind(this);
+  render(){
+    return(
+      <div>
+        <h1>Context</h1>
+        <Title />
+        <Message />
+      </div>
+    );
   }
+}
 
-  doCheck(event){
-    alert(event.target.value + "は長すぎます。")
-  }
+class Title extends Component {
+  static contextType = SampleContext;
 
   render(){
     return(
       <div>
-        <h1>React</h1>
-        <h2>{this.state.message}</h2>
-        <Message maxlengh="10" onCheck={this.doCheck} />
+        <h2>{this.context.title}</h2>
       </div>
     );
   }
 }
 
 class Message extends Component {
-  constructor(props){
-    super(props);
-    this.doChange = this.doChange.bind(this);
-  }
-
-  doChange(e){
-    if (e.target.value.length > this.props.maxlength){
-      this.props.onCheck(e);
-      e.target.value = 
-      e.target.value.substr(0, this.props.maxlength);
-    }
-  }
+  static contextType = SampleContext;
 
   render(){
     return(
-      <input type="text" onChange={this.doChange} />
+      <div>
+        <p>{this.context.message}</p>
+      </div>
     );
   }
 }
